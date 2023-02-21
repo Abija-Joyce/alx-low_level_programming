d21151ac567485f4f7f5b81a298d97bc23dcf3cc
Abija-Joyce <joyce.abija@eng.uniben.edu>
2023-02-21 09:31:19 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Prints out absolute of input
+ * @t: The number
+ *
+ * Return: Always 0
+*/
+
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
