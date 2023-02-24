88f101117d15d6774b9b6d52a8a4d318615f31d3
Abija-Joyce <joyce.abija@eng.uniben.edu>
2023-02-24 15:15:47 -0800

    Task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - Prints line with character '_'
+ * @n: Char to b printed
+ *
+*/
+
+void print_line(int n)
+{
+	int line:
+
+		if (n > 0)
+		{
+			for (line = 0; line <= n; line++)
+				_putchar ('_');
+		}
+		_putchar ('\n');
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal with char \.
+ * @n: Char
+*/
+
+void print_diagonal(iny n)
+{
+	int line, gap;
+
+		if (n > 0)
+		{
+			for (line = 0; line <= n; line++)
+			{
+				for (gap = 0; gap <= n; gap++)
+				{
+
+					_putchar (' ');
+				}
+				_putchar ('\\');
+				if (line == n - 1)
+					continue;
+				_putchar ('\n');
+			}
+		}
+		_putchar ('\n');
+}
