f4abf4f2fc22840b95b32270e055dbd0a3d03d58
Abija-Joyce <joyce.abija@eng.uniben.edu>
2023-03-22 08:12:39 -0700

    C-0x0F-functions_pointers

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+C- 0x0F Functions_Pointers
+
+C Task on Function Pointers.
---
+++ 0x0F-function_pointers/_putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * _putchar - Prints out
+ * @c: The char
+ * Return: Nothing
+*/
+int _putchar(char c)
+{
+	return (write (1, &c, 1));
+	}
