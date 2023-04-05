c0042beddb93cbdfe84db29261a3ca8c351e364f
Abija-Joyce <joyce.abija@eng.uniben.edu>
2023-04-05 06:32:07 -0700

    C- Singly linked Lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,7 +15,19 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-} listint_t;
+}listint_t;
+
+/**
+ * struct listp_s - Pointer List
+ * @p: Pointer
+ * @next: Point to the next node
+*/
+
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
